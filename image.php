c77531e7ee1b33aada6722bbf4c24eb0802ae5aa
thomas1568 <144383335+thomas1568@users.noreply.github.com>
2025-01-20 18:54:18 +0100

    Update image.php

--- image.php
+++ image.php
@@ -131,14 +131,14 @@
 
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="index.php">THOMAS</a>
+            <a class="navbar-brand" href="index.html">THOMAS</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="index.php">Accueil</a>
+                        <a class="nav-link active" aria-current="page" href="index.html">Accueil</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="image.php">Portfolio</a>
